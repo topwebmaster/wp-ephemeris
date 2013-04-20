259972f0f0f68fc8d78f1ca915c157ecf55ebc8c
Andrew S. Freeman <andrew@andrewsfreeman.com>
2013-04-20 14:41:57 -0700

    Works on my site

--- wp-ephemeris.php
+++ wp-ephemeris.php
@@ -9,54 +9,66 @@ Author URI: http://andrewsfreeman.com
 License: GPL2
 */
 
-$planets = array(
-	0 => 'Sun',
-	1 => 'Moon',
-	2 => 'Mercury',
-	3 => 'Venus',
-    4 => 'Mars',
-    5 => 'Jupiter',
-	6 => 'Saturn',
-	7 => 'Uranus',
-	8 => 'Neptune',
-	9 => 'Pluto',
-	10 => 'mean Node',
-	11 => 'true Node',
-	12 => 'mean Apogee'
-);
+class WPephemeris {
+	public $planets = array(
+		0 => 'Sun',
+		1 => 'Moon',
+		2 => 'Mercury',
+		3 => 'Venus',
+	    4 => 'Mars',
+	    5 => 'Jupiter',
+		6 => 'Saturn',
+		7 => 'Uranus',
+		8 => 'Neptune',
+		9 => 'Pluto',
+		10 => 'mean Node',
+		11 => 'true Node',
+		12 => 'mean Apogee'
+	);
 
-$zodiac = array(
-	'ar' => 'Aries',
-	'ta' => 'Taurus',
-	'ge' => 'Gemini',
-	'cn' => 'Cancer',
-	'le' => 'Leo',
-	'vi' => 'Virgo',
-	'li' => 'Libra',
-	'sc' => 'Scorpio',
-	'sa' => 'Sagittarius',
-	'cp' => 'Capricorn',
-	'aq' => 'Aquarius',
-	'pi' => 'Pisces',
-);
+	public $zodiac = array(
+		'ar' => 'Aries',
+		'ta' => 'Taurus',
+		'ge' => 'Gemini',
+		'cn' => 'Cancer',
+		'le' => 'Leo',
+		'vi' => 'Virgo',
+		'li' => 'Libra',
+		'sc' => 'Scorpio',
+		'sa' => 'Sagittarius',
+		'cp' => 'Capricorn',
+		'aq' => 'Aquarius',
+		'pi' => 'Pisces'
+	);
 
-# run swetest with date information and separate out results by newlines
-$result = shell_exec('./src/swetest -b29.05.1991 -t12.000 -fTZ -roundmin -head');
-$chart = explode( "\n", $result );
+	public function __construct() {
+	}
 
-# trim excess information
-foreach ( $chart as $index => $planet ) :
-	$chart[$index] = substr( $planet, 23 );
-endforeach;
+	public function print_my_zodiac( $atts ) {
+		# run swetest with date information and separate out results by newlines
+		$result = `/var/www/andrewsfreeman.com/public/wp-content/plugins/wp-ephemeris/src/swetest -b29.05.1991 -t12.000 -fTZ -roundmin -head 2>&1`;
+		$chart = explode( "\n", $result );
+		# trim excess information
+		foreach ( $chart as $index => $planet ) :
+			$chart[$index] = substr( $planet, 23 );
+		endforeach;
 
+		$ephem = new WPephemeris;
 
-# only get planets
-$chart = array_slice( $chart , 0, 13 );
+		# only get planets
+		$chart = array_slice( $chart , 0, 13 );
 
-# print it all baby!
-foreach( $planets as $index => $planet ) :
-	$deg = substr( $chart[$index], 0, 2 );
-	$sign = substr( $chart[$index], 3, 2 );
-	echo $planet . " is " . $deg . " degrees " . $zodiac[$sign] .'.';
-	echo "\n";
-endforeach;
+		$output = "";
+		# print it all baby!
+		foreach( $ephem->planets as $index => $planet ) :
+			$deg = substr( $chart[$index], 0, 2 );
+			$sign = substr( $chart[$index], 3, 2 );
+			$output .= '<br />' . $planet . " is " . $deg . " degrees " . $ephem->zodiac[$sign] .'.';
+		endforeach;
+		return $output;
+	}
+}
+
+new WPephemeris;
+
+add_shortcode( 'asf-zodiac', array( 'WPephemeris', 'print_my_zodiac' ) );
